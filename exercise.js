bfbfc45f6ef0798cc0df51953076ccf4e337663b
dipillu65 <dipodoherty@gmail.com>
2019-02-24 22:34:34 -0500

    HW @ Completed

--- exercise.js
+++ exercise.js
@@ -1,54 +1,61 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
-    //-------------------------------------------
-    // YOUR CODE
     // Return the total number of records
-    //-------------------------------------------
-    return 'Error: countRecords not implemented'; 
+
+    var countRecords = 0;
+    for (i=0 ; i < data.length; i++){
+        countRecords++;
+    } return countRecords;
+    
 };
 
 exercise.countDistrictCrimes = function(data,district){
-    //-------------------------------------------
-    // YOUR CODE
     // Return the number of crimes 
     // for the given district
-    //-------------------------------------------
-    return 'Error: countDistrictCrimes not implemented'; 
+    var districtCount = 0;
+    for (i=0 ; i < data.length; i++){
+        if(data[i][19]===district){
+            districtCount++;
+        }
+    }return districtCount;
+   
 };
 
 exercise.countPrimaryType = function(data,primaryType){
-    //-------------------------------------------
-    // YOUR CODE
     // Return the number of crimes 
     // for the giving primary type
-    //-------------------------------------------
-    return 'Error: countPrimaryType not implemented';
+
+    var numCrimes = 0;
+    for (i=0 ; i < data.length; i++){
+        if(data[i][13]===primaryType){
+            numCrimes++;
+        }
+    }return numCrimes;
+
 };
 
 exercise.countLocation = function(data,location){
-    //-------------------------------------------
-    // YOUR CODE
     // Return the number of crimes 
     // for the given location
-    //-------------------------------------------
-    return 'Error: countLocation not implemented';
+    var numCrimesLoc = 0;
+    for (i=0 ; i < data.length; i++){
+        if(data[i][15]==location){
+            numCrimesLoc++;
+        }
+    }return numCrimesLoc;
+    
+
 };
 
 
 exercise.buildLatLngPoint = function(crime){
-    //-------------------------------------------
-    // YOUR CODE
     // build the point for the given crime
-    // 
-    // For example:
-    //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
-    //-------------------------------------------    
-    return 'Error: buildLatLngPoint not implemented';
+    var point = {};    
+    point.latitude = crime[26];
+    point.longitude = crime[28];
+    return point;    
+
 };
 
 
